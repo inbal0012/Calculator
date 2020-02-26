3115bd19ecd43f0b1c031a929cfa0550f7b89b74
Inbal Schkolnik <capoeira0012>
2020-02-26 10:59:00 +0200

    number handeled, including a display check

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.calculator_keys');
+const display = calculator.querySelector('.calculator_display');
 
 keys.addEventListener('click', e => {
   if (e.target.matches('button')) {
     const key = e.target;
     const action = key.dataset.action;
+    const keyContent = key.textContent;
+    let displayedNum = display.textContent;
 
     if (!action) {
-      console.log('number key!');
+      if (displayedNum === '0') {
+        display.textContent = keyContent;
+      } else {
+        display.textContent = displayedNum + keyContent;
+      }
+      console.log('number key! ' + keyContent);
     }
 
     if (
